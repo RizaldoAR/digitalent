49c317ba56072c2074b8633a6498cefe990c740f
rizaldoar <rizaldo.a.r@gmail.com>
2024-08-27 15:22:59 +0700

    Menambahkan page home

---
+++ app/Views/layout/header.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+</head>
+
+
+
+<nav class="navbar navbar-expand-lg  navbar-dark bg-dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+            <div class="d-flex flex-row">
+                <img src="<?= base_url('img/logo.png'); ?>" alt="" width="60" height="60" class="d-inline-block align-text-top">
+            </div>
+        </a>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <div class="navbar-nav">
+                <a class="nav-link active" aria-current="page" href="#">Beranda</a>
+                <a class="nav-link" href="#">Package List</a>
+                <a class="nav-link" href="#">Order List</a>
+            </div>
+        </div>
+    </div>
+
+
+
+</nav>
+
+
+
+
+
+
+<body>
